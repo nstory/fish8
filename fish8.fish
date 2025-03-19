8cb69585256ce6c68a5b0176122984a2bab902a7
Nathaniel Story <nathanielstory@gmail.com>
2025-03-19 09:40:05 -0400

    ...

--- fish8.fish
+++ fish8.fish
@@ -1,175 +1,111 @@
 #!/usr/bin/env fish
 
 # constants
-set memory_size 4096
-set program_start (math 0x200)
-set stack_limit 256
-set display_width 64
-set display_height 32
-set speed 720 # 700 cycles (instructions) per second
-set cycles_per_tick (math $speed / 60)
+set -g memory_size 4096
+set -g program_start (math 0x200)
+set -g stack_limit 256
+set -g display_width 64
+set -g display_height 32
+set -g speed 720 # 700 cycles (instructions) per second
+set -g cycles_per_tick (math $speed / 60)
 
-# initialize memory for programs; MEMORY[0] doesn't actually exist b/c fish
-# is one-indexed, but I don't think programs will care; at each address is
-# stored the value as a string decimal number lol
-set memory ( jot -b 0 (math $memory_size - 1) )
-
-# initialize the rest
-set stack
-set reg_pc $program_start
-set reg_i
-set reg_delay
-set reg_sound
-set reg_v ( jot -b 0 16 )
-set display ( jot -b 0 (math "$display_width * $display_height") ) # 64x32 pixels
-
-# fixme: load font into memory!
-
-argparse "stop-after=" "draw-at-end" -- $argv
-if set -q _flag_stop_after
-  set stop_after $_flag_stop_after
-else
-  set stop_after -1
+function main
+  parse_args $argv
+  initialize
+  load_program
+  main_loop
 end
 
-if set -q _flag_draw_at_end
-  set draw_at_end
+function initialize
+  # initialize memory for programs; MEMORY[0] doesn't actually exist b/c fish
+  # is one-indexed, but I don't think programs will care; at each address is
+  # stored the value as a string decimal number lol
+  set -g memory ( jot -b 0 (math $memory_size - 1) )
+
+  # initialize the rest
+  set -g stack
+  set -g reg_pc $program_start
+  set -g reg_i
+  set -g reg_delay
+  set -g reg_sound
+  set -g reg_v ( jot -b 0 16 )
+  set -g display ( jot -b 0 (math "$display_width * $display_height") ) # 64x32 pixels
+  # fixme: load font into memory!
 end
 
-# load program into memory
-set program_file $argv[1]
-if test -z $program_file
-  echo "Usage: fish8.fish program_file"
-  exit 1
-end
-if test ! -e $program_file
-  echo "Couldn't load program file \"$program_file\""
-  exit 1
-end
-# can't directly read binary file with read :shrug: dunno why so I need to
-# convert it into hex, and then read that, lol
-set program_hex ( od -v -t x1 < $program_file | sed -e 's/^[0-9]* *//g' | tr '\n' ' ' | sed -e 's/  */ /g' | sed -e 's/ *$//' )
-set program_dec
-for hex in (string split ' ' $program_hex)
-  set -a program_dec (math "0x$hex")
-end
-for i in (seq (count $program_dec))
-  set index (math $program_start + $i - 1)
-  set memory[$index] $program_dec[$i]
-end
-
-function blit_display_half_block
-  echo -ne '\e[H' # go to top left
-  for y in (seq 0 2 (math $display_height - 1))
-    for x in (seq 1 $display_width)
-      set -l upper $display[(math "$y*$display_width + $x")]
-      set -l lower $display[(math "$y*$display_width + $display_width + $x")]
-      if test -z $upper
-        reset
-        echo $y $display_width $x
-        exit
-      end
-      if test $upper -eq 1
-        printf "\e[38;5;255m"
-      else
-        printf "\e[38;5;0m"
-      end
-      if test $lower -eq 1
-        printf "\e[48;5;255m"
-      else
-        printf "\e[48;5;0m"
-      end
-      printf "\u2580"
-    end
-    echo
+function parse_args
+  argparse "stop-after=" "draw-at-end" -- $argv
+  if set -q _flag_stop_after
+    set -g stop_after $_flag_stop_after
+  else
+    set -g stop_after -1
   end
-  printf "\e[1;0m"
-  printf "pc: 0x%03x "  $reg_pc
-  printf "i: 0x%03x " $reg_i
-  printf "sk: "
-  for s in $stack
-    printf "0x%04x " $s
+
+  if set -q _flag_draw_at_end
+    set -g draw_at_end
+  end
+
+  # load program into memory
+  set -g program_file $argv[1]
+  if test -z $program_file
+    echo "Usage: fish8.fish program_file"
+    exit 1
+  end
+  if test ! -e $program_file
+    echo "Couldn't load program file \"$program_file\""
+    exit 1
   end
 end
 
-function blit_display_simple
-  for i in (seq (count $display))
-    if test $display[$i] -eq 1
-      echo -ne '#'
-    else
-      echo -n ' '
+function load_program
+  # can't directly read binary file with read :shrug: dunno why so I need to
+  # convert it into hex, and then read that, lol
+  set program_hex ( od -v -t x1 < $program_file | sed -e 's/^[0-9]* *//g' | tr '\n' ' ' | sed -e 's/  */ /g' | sed -e 's/ *$//' )
+  set program_dec
+  for hex in (string split ' ' $program_hex)
+    set -a program_dec (math "0x$hex")
+  end
+  for i in (seq (count $program_dec))
+    set index (math $program_start + $i - 1)
+    set memory[$index] $program_dec[$i]
+  end
+end
+
+function main_loop
+  set -g cycle 0 # number of cycles since start
+
+  # fetch-decode-execute ad infinitum
+  while :
+    set cycle (math $cycle + 1)
+    if test $stop_after -ne -1 -a $cycle -gt $stop_after
+      break
     end
-    if test (math "($i - 1)%$display_width") -eq (math $display_width - 1)
-      printf "\n"
+
+    fetch
+    decode_execute
+
+    if ! set -q draw_at_end
+      blit_display_half_block
     end
   end
+
+  if set -q draw_at_end
+    blit_display_simple
+  end
 end
 
-function draw -a x y height index
-  set reg_v[15] 0
-  set x (math $x % $display_width)
-  set y (math $y % $display_height)
-  for yi in (seq $y (math "min($y+$height-1, $display_height-1)"))
-    set -l sprite_row $memory[$index]
-    set index (math $index + 1)
-    for xi in (seq $x (math "min($x+8-1, $display_width-1)"))
-      set display_idx (math "$yi * $display_width + $xi + 1")
-      set old_pixel $display[$display_idx]
-      set new_pixel (math "bitand($sprite_row, 0x80) / 0x80")
-
-      if test $old_pixel -eq 1 -a $new_pixel -eq 1
-        set display[$display_idx] 0
-        set reg_v[15] 1
-      else if test $new_pixel -eq 1
-        set display[$display_idx] 1
-      end
-
-      set sprite_row (math "$sprite_row * 2")
-    end
-  end
-  # if ! set -q draw_at_end
-  #   blit_display_half_block
-  # end
-end
-
-function subtract -a a b
-  set -f flag 0
-  if test $a -ge $b
-    set -f flag 1
-  end
-  set reg_v[$x] (math $a - $b)
-  if test $flag -eq 0
-    set reg_v[$x] (math $reg_v[$x] + 256)
-  end
-  set reg_v[15] flag
-end
-
-function crash
-  printf "  cpu state: pc=0x%04x i=0x%02x\n" $reg_pc $reg_i
-  printf "instruction: hi_byte=0x%02x lo_byte=0x%02x x=0x%02x y=0x%02x nn=0x%02x nnn=0x%03x\n" $hi_byte $lo_byte $x $y $nn $nnn
-  echo "crash! $argv"
-  exit 1
-end
-
-set cycle 0 # number of cycles since start
-
-# fetch-decode-execute ad infinitum
-while :
-  set hi_byte $memory[$reg_pc]
-  set hi_nibble (math "bitand($hi_byte, 0xF0)/16")
-  set reg_pc (math $reg_pc + 1)
-  set lo_byte $memory[$reg_pc]
-  set reg_pc (math $reg_pc + 1)
-  set x (math bitand $hi_byte, 0x0F)
-  set real_x $x # without the 16 hack later on
-  set y (math "bitand($lo_byte, 0xF0)/16")
-  set cycle (math $cycle + 1)
-
-  if test $stop_after -ne -1 -a $cycle -gt $stop_after
-    break
-  end
+# parse the instruction at reg_pc and increment reg_pc
+function fetch
+  set -g hi_byte $memory[$reg_pc]
+  set -g hi_nibble (math "bitand($hi_byte, 0xF0)/16")
+  set -g reg_pc (math $reg_pc + 1)
+  set -g lo_byte $memory[$reg_pc]
+  set -g reg_pc (math $reg_pc + 1)
+  set -g x (math bitand $hi_byte, 0x0F)
+  set -g y (math "bitand($lo_byte, 0xF0)/16")
 
   # arrays 1-indexed so reg 0 is stored at 16 instead lol
+  set -g real_x $x # without the 16 hack
   if test $x = 0
     set x 16
   end
@@ -177,9 +113,12 @@ while :
     set y 16
   end
 
-  set n (math "bitand($lo_byte, 0xF)")
-  set nn $lo_byte
-  set nnn (math "bitand($hi_byte, 0x0f)*256+$lo_byte")
+  set -g n (math "bitand($lo_byte, 0xF)")
+  set -g nn $lo_byte
+  set -g nnn (math "bitand($hi_byte, 0x0f)*256+$lo_byte")
+end
+
+function decode_execute
   if test $hi_byte -eq 0
     if test $lo_byte -eq (math 0xe0)
       # clear screen
@@ -291,14 +230,102 @@ while :
       crash "unknown 0xF0 code:" (printf "0x%02x" $lo_byte)
     end
   else
-      crash "unknown opcode" (printf "0x%x" $hi_nibble)
-  end
-
-  if ! set -q draw_at_end
-    blit_display_half_block
+    crash "unknown opcode" (printf "0x%x" $hi_nibble)
   end
 end
 
-if set -q draw_at_end
-  blit_display_simple
+function blit_display_half_block
+  echo -ne '\e[H' # go to top left
+  for y in (seq 0 2 (math $display_height - 1))
+    for x in (seq 1 $display_width)
+      set -l upper $display[(math "$y*$display_width + $x")]
+      set -l lower $display[(math "$y*$display_width + $display_width + $x")]
+      if test -z $upper
+        reset
+        echo $y $display_width $x
+        exit
+      end
+      if test $upper -eq 1
+        printf "\e[38;5;255m"
+      else
+        printf "\e[38;5;0m"
+      end
+      if test $lower -eq 1
+        printf "\e[48;5;255m"
+      else
+        printf "\e[48;5;0m"
+      end
+      printf "\u2580"
+    end
+    echo
+  end
+  printf "\e[1;0m"
+  printf "pc: 0x%03x "  $reg_pc
+  printf "i: 0x%03x " $reg_i
+  printf "sk: "
+  for s in $stack
+    printf "0x%04x " $s
+  end
 end
+
+function blit_display_simple
+  for i in (seq (count $display))
+    if test $display[$i] -eq 1
+      echo -ne '#'
+    else
+      echo -n ' '
+    end
+    if test (math "($i - 1)%$display_width") -eq (math $display_width - 1)
+      printf "\n"
+    end
+  end
+end
+
+function draw -a x y height index
+  set reg_v[15] 0
+  set x (math $x % $display_width)
+  set y (math $y % $display_height)
+  for yi in (seq $y (math "min($y+$height-1, $display_height-1)"))
+    set -l sprite_row $memory[$index]
+    set index (math $index + 1)
+    for xi in (seq $x (math "min($x+8-1, $display_width-1)"))
+      set display_idx (math "$yi * $display_width + $xi + 1")
+      set old_pixel $display[$display_idx]
+      set new_pixel (math "bitand($sprite_row, 0x80) / 0x80")
+
+      if test $old_pixel -eq 1 -a $new_pixel -eq 1
+        set display[$display_idx] 0
+        set reg_v[15] 1
+      else if test $new_pixel -eq 1
+        set display[$display_idx] 1
+      end
+
+      set sprite_row (math "$sprite_row * 2")
+    end
+  end
+  # if ! set -q draw_at_end
+  #   blit_display_half_block
+  # end
+end
+
+function subtract -a a b
+  set -f flag 0
+  if test $a -ge $b
+    set -f flag 1
+  end
+  set reg_v[$x] (math $a - $b)
+  if test $flag -eq 0
+    set reg_v[$x] (math $reg_v[$x] + 256)
+  end
+  set reg_v[15] flag
+end
+
+function crash
+  printf "  cpu state: pc=0x%04x i=0x%02x\n" $reg_pc $reg_i
+  printf "instruction: hi_byte=0x%02x lo_byte=0x%02x x=0x%02x y=0x%02x nn=0x%02x nnn=0x%03x\n" $hi_byte $lo_byte $x $y $nn $nnn
+  echo "crash! $argv"
+  exit 1
+end
+
+main $argv
+
